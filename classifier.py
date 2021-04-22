762dbc4650c8a07ce048e5780c988e5b80cc6f23
Maciej Kaczkowski <maciej.kaczkowski.stud@pw.edu.pl>
2021-04-22 17:33:37 +0200

    adding column names

--- classifier.py
+++ classifier.py
@@ -14,6 +14,10 @@ train_df = pd.read_csv(r'seeds_dataset_clean.txt', header=None, sep='\t')
 # train_df = pd.read_csv(r'seeds_dataset.txt', header=None, sep='\n', skipinitialspace=True)
 # train_df = train_df[0].str.split('\t', expand=True)
 
-print(train_df.head())
+# pre-training operations
+train_df.columns = ['area', 'perimeter', 'compactness', 'kernel length',
+                    'kernel width', 'asymmetry coef.', 'groove length', 'class']
+
+print(train_df.head().to_string())
 print(train_df.info())
 print(train_df.describe())
