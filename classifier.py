ec902395e4c8468149c65872d86e3b997c2f6f79
Maciej Kaczkowski <maciej.kaczkowski.stud@pw.edu.pl>
2021-04-29 12:06:39 +0200

    nbc appears to be working...

--- classifier.py
+++ classifier.py
@@ -26,7 +26,7 @@ class NaiveBayesClassifier:
         # init mean, var, priors
         self._mean = np.zeros((n_classes, n_features), dtype=np.float64)
         self._var = np.zeros((n_classes, n_features), dtype=np.float64)
-        self._priors = np.zeros((n_classes, n_features), dtype=np.float64)
+        self._priors = np.zeros(n_classes, dtype=np.float64)
         # print(self.mean.size)
         # print(self.var.size)
         # print(self.priors.size)
@@ -40,33 +40,35 @@ class NaiveBayesClassifier:
             self._priors[c-1] = X_c.shape[0] / float(n_samples)
 
         # print('mean:\n')
-        # print(self.mean)
+        # print(self._mean)
         # print('var:\n')
-        # print(self.var)
+        # print(self._var)
+        # print('priors:\n')
+        # print(self._priors)
 
     def predict(self, X):
-        # TODO: implement iterating over rows - pass propor argument (ndarray??) - how to change pd.DataFrame row into ndarray
-        y_pred = [self.predict_single_item(x) for x in X]
+        X_matrix = X.to_numpy()
+        y_pred = [self.predict_single_item(x) for x in X_matrix]
+        # print(y_pred)
         return y_pred
 
-    # TODO: implement using notes, especially priors. Is iterating over features needed - I think no, check formula
     def predict_single_item(self, x):
         posteriors = []
 
         for cls_idx, c in enumerate(self.classes):
-            for feat_idx, f in enumerate(self.features):
-                prior = np.log(self._priors[cls_idx, feat_idx])
-                class_conditional = np.sum(np.log(self._pdf(cls_idx, feat_idx, x)))
-                posterior = prior + class_conditional
-                posteriors.append(posterior)
+            prior = np.log(self._priors[cls_idx])
+            class_conditional = np.sum(np.log(self._pdf(cls_idx, x)))
+            posterior = prior + class_conditional
+            posteriors.append(posterior)
 
         return self.classes[np.argmax(posteriors)]
 
     #TODO: fix using notes
-    def _pdf(self, class_idx, feat_idx, x):
-        mean = self._mean[class_idx, feat_idx]
-        var = self._var[class_idx, feat_idx]
-        print(var, mean, x)
+    def _pdf(self, class_idx, x):
+        mean = self._mean[class_idx]
+        var = self._var[class_idx]
+        # print('var, mean, x:')
+        # print(var, mean, x)
         numerator = np.exp(- (x-mean)**2 / (2 * var))
         denominator = np.sqrt(2 * np.pi * var)
         return numerator / denominator
--- tests.py
+++ tests.py
@@ -4,6 +4,7 @@ Author: Maciej Kaczkowski
 '''
 
 
+import pandas as pd
 import numpy as np
 import scipy.stats
 
@@ -19,6 +20,7 @@ def pdf(x):
 print(scipy.stats.norm(11.3, 0.01).pdf(11.31))
 print(pdf(11.31))
 
-
-
-
+# Dummy DataFrame
+df = pd.DataFrame({'col1': [1,2,3], 'col2': [4,5,6]})
+# Extract second row (index: 1)
+print(df.iloc[1].to_numpy())
